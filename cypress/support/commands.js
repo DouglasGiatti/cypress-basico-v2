40fc513912bd4a3722b2b5f8632767bf3aab3e84
Douglas Giatti <douglas_giatti@hotmail.comm>
2024-02-21 23:31:00 -0300

    Break the app on purpose

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
     cy.get('#firstName').type('Douglas')
     cy.get('#lastName').type('Giatti')
-    cy.get('#email').type('douglas_giatti@hotmail,com')
+    cy.get('#email').type('douglas_giatti@hotmail.com')
     cy.get('#phone-checkbox').click()
     cy.get('#open-text-area').type('Teste')
     cy.contains('button', 'Enviar').click()
